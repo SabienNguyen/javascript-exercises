b0d1810ab2fadc3537a1195c1001527acda3ece2
Sabien Nguyen <sabien.q.nguyen@gmail.com>
2024-11-11 17:46:03 -0800

    complete 1-4 exercises js foundations

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(vals, ...args) {
+    for (let j = 0; j < args.length; j++) {
+        for (let i = 0; i < vals.length; i++) {
+            if(vals[i] === args[j]) {
+                vals.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return vals;
 };
 
 // Do not edit below this line
