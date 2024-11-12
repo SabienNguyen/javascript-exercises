836bdfc3e954994e727a0573983eaf532b47fb32
Sabien Nguyen <sabien.q.nguyen@gmail.com>
2024-11-11 18:24:19 -0800

    complete 5-7 js exercises foundation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }else if (!Number.isInteger(num1) || !Number.isInteger(num2) ) {
+        return 'ERROR';
+    }
 
+    start = num1 < num2 ? num1 : num2;
+    end = num1 < num2 ? num2 : num1;
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
